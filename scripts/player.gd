a6f7e565a633e59614e80eea0faa0e18bbcadc21
WILLIAM NUNES GUERREIROS <williamnunes182@gmail.com>
2024-09-16 20:42:41 -0300

    zero machine states

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,12 +2,12 @@ extends CharacterBody2D
 class_name Player
 
 
-
 @onready var spriteanim: AnimatedSprite2D = $spriteanim
 @onready var player: Player = $"."
 
 
-
+func _ready() -> void:
+	pass
 
 func _physics_process(_delta: float) -> void:
 	move_and_slide()
