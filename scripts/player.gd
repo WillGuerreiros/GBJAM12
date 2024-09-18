5ecaf570a3f9b7939dd6c24fd32c7aec7c1b04c0
WILLIAM NUNES GUERREIROS <williamnunes182@gmail.com>
2024-09-18 10:07:46 -0300

    states_new_aproach

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,9 +5,15 @@ class_name Player
 @onready var spriteanim: AnimatedSprite2D = $spriteanim
 @onready var player: Player = $"."
 
+var state = ["idle_down","pickup_down","walk_down"]
+
 
 func _ready() -> void:
 	pass
 
 func _physics_process(_delta: float) -> void:
+	check_states()
 	move_and_slide()
+
+func check_states():
+	pass
