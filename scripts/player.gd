19279d74a3d6e5b537738e1d83f146c5f9e9d237
WILLIAM NUNES GUERREIROS <williamnunes182@gmail.com>
2024-09-19 12:04:38 -0300

    add movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,15 +5,48 @@ class_name Player
 @onready var spriteanim: AnimatedSprite2D = $spriteanim
 @onready var player: Player = $"."
 
-var state = ["idle_down","pickup_down","walk_down"]
+enum states {IDLE_DOWN, PICKUP_DOWN, WALK_DOWN}
 
+
+@export var currentState :states
+@export var speed: float 
+@export var accel: float
 
 func _ready() -> void:
 	pass
 
 func _physics_process(_delta: float) -> void:
-	check_states()
+	
+	if Input.is_action_pressed("down_button") :
+		currentState = states.WALK_DOWN
+	elif Input.is_action_just_released("down_button"):
+		currentState = states.IDLE_DOWN
+	
+	match currentState:
+		states.IDLE_DOWN:
+			idle_down_function()
+		states.PICKUP_DOWN:
+			pickup_down_function()
+		states.WALK_DOWN:
+			walk_down_function()
 	move_and_slide()
 
-func check_states():
+func change_state(newState):
+	currentState = newState
+	pass
+
+func idle_down_function():
+	spriteanim.play("idle_animation")
+	if player.velocity > Vector2.ZERO:
+		print(player.velocity)
+		player.velocity -= player.velocity*accel
+
+func walk_down_function():
+	spriteanim.play("walk_down_animation")
+	if velocity.y < speed:
+		player.velocity.y += 1 + velocity.y*accel
+		print(player.velocity)
+	
+
+func pickup_down_function():
 	pass
